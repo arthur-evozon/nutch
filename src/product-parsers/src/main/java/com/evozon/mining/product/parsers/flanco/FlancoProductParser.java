b946cdbbf1af5475c85b6055648084fd07904002
Arthur Bidian <arthur.bidian@evozon.com>
2016-05-23 17:26:46 +0300

    Added Flanco product parser

---
+++ src/product-parsers/src/main/java/com/evozon/mining/product/parsers/flanco/FlancoProductParser.java
@@ -0,0 +1,52 @@
+package com.evozon.mining.product.parsers.flanco;
+
+import com.evozon.mining.product.parsers.DefaultProductParser;
+import com.evozon.mining.product.parsers.ProductParser;
+import org.apache.nutch.storage.WebPage;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class FlancoProductParser extends DefaultProductParser implements ProductParser {
+	private static final Logger LOG = LoggerFactory.getLogger(FlancoProductParser.class);
+
+	@Override
+	protected String parseProductMeta(String url, WebPage page, Document document, String metaSelectors) {
+		StringBuilder metaLines = new StringBuilder();
+
+		Elements valueElements = document.select(metaSelectors);
+
+		if (valueElements != null) {
+			for (Element valueElement : valueElements) {
+				Element keyElement = valueElement.previousElementSibling();
+				List<Node> keyElementText = keyElement.childNodes();
+
+				if (keyElementText == null || keyElementText.size() != 1) {
+					continue;
+				}
+
+				String key = keyElementText.get(0).toString().trim();
+				key = removeTrailing(":", key);
+
+				List<Node> valueElementText = valueElement.childNodes();
+
+				if (valueElementText == null || valueElementText.size() != 1) {
+					continue;
+				}
+				String value = valueElementText.get(0).toString().trim();
+
+				if (metaLines.length() > 0) {
+					metaLines.append("\n");
+				}
+				metaLines.append(key + ":" + value);
+			}
+		}
+
+		return metaLines.toString();
+	}
+}
