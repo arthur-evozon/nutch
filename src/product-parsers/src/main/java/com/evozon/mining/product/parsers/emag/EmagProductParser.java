bbf45eb59da53c0eb4a41143839156b185bf2ea6
Arthur Bidian <arthur.bidian@evozon.com>
2016-05-27 17:09:09 +0300

    updated Emag Parser

--- src/product-parsers/src/main/java/com/evozon/mining/product/parsers/emag/EmagProductParser.java
+++ src/product-parsers/src/main/java/com/evozon/mining/product/parsers/emag/EmagProductParser.java
@@ -9,11 +9,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EmagProductParser extends DefaultProductParser implements ProductParser {
-
 	private static final Logger LOG = LoggerFactory.getLogger(EmagProductParser.class);
+
+	static final String[] DETAILS_NESTED_SELECT_PATTERN = new String[]{"tbody", "tr", "td"};
+
+	private final String[] detailsNestedSelectPattern;
+
+	public EmagProductParser() {
+		this(DETAILS_NESTED_SELECT_PATTERN);
+	}
+
+	public EmagProductParser(String... detailsNestedSelectPattern) {
+		this.detailsNestedSelectPattern = detailsNestedSelectPattern;
+	}
 
 	@Override
 	protected String parseProductMeta(String url, WebPage page, Document document, String metaSelectors) {
-		return ProductParserUtils.buildNameValuesString(ProductParserUtils.getAllNameValuesBySelector(document, metaSelectors));
+		return ProductParserUtils.buildNameValuesString(ProductParserUtils.getAllNameValuesWithSelectorPattern(document, metaSelectors,
+				detailsNestedSelectPattern));
 	}
 }
